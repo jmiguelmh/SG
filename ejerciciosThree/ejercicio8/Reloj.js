5073c8c72bcb3aeb737aefd78e81ca13292c55fc
Jose Miguel Marquez Herreros <jmiguelmh3000@gmail.com>
2021-04-25 18:08:00 +0200

    Ejercicios Three

---
+++ ejerciciosThree/ejercicio8/Reloj.js
@@ -0,0 +1,18 @@
+import * as THREE from '../libs/three.module.js'
+
+class Reloj extends THREE.Object3D {
+  constructor(gui, titleGui) {
+    super();
+
+  }
+
+  createGUI (gui, titleGui) {
+
+  }
+
+  update() {
+
+  }
+}
+
+export { Reloj }
